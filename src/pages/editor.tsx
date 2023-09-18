005b1be1a956423fedb8ec780a3795966f7995a4
miguel1man <miguelangelhh17@gmail.com>
2023-09-18 13:50:33 -0500

    fix getDataURL()

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -1,11 +1,12 @@
 import React, { useRef } from "react"
-import ReactDOM from "react-dom"
 import CanvasDraw from "react-canvas-draw"
 
 const EditorPage = () => {
   const canvasRef = useRef(null)
   const handleClick = () => {
-    console.log("Link:", canvasRef.current && canvasRef.current.getDataURL())
+    // @ts-ignore
+    const imageLink = canvasRef.current?.getDataURL()
+    imageLink && console.log("Link:", imageLink)
   }
 
   return (
@@ -13,8 +14,6 @@ const EditorPage = () => {
       <p>Editor</p>
       <CanvasDraw
         ref={canvasRef}
-        hideGridX={false}
-        hideGridY={false}
         imgSrc="https://plus.unsplash.com/premium_photo-1663054378169-8ffea2e11c42?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
       />
       <button onClick={handleClick}>Save</button>
