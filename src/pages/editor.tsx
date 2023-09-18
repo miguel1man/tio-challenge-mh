c5742fe354561878206560260b99ec6ad06d208d
miguel1man <miguelangelhh17@gmail.com>
2023-09-18 13:59:43 -0500

    fix CanvasDraw children error

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -11,12 +11,14 @@ const EditorPage = () => {
 
   return (
     <>
-      <p>Editor</p>
-      <CanvasDraw
-        ref={canvasRef}
-        imgSrc="https://plus.unsplash.com/premium_photo-1663054378169-8ffea2e11c42?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
-      />
-      <button onClick={handleClick}>Save</button>
+      {
+        // @ts-ignore
+        <CanvasDraw
+          ref={canvasRef}
+          imgSrc="https://plus.unsplash.com/premium_photo-1663054378169-8ffea2e11c42?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
+        />
+      }
+      <button onClick={handleClick}>Get Image Link</button>
     </>
   )
 }
